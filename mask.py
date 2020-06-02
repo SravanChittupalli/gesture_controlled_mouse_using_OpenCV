32de9f57774d1760b127314c1ce89763fc7169fd
SravanChittupalli <sravanchittupalli7@gmail.com>
2020-06-02 12:11:19 +0530

    checked for red

--- mask.py
+++ mask.py
@@ -3,8 +3,8 @@ import numpy as np
 import mouse
 
 vid = cv2.VideoCapture(0)
-lower_red = np.array([75, 30, 62])
-upper_red = np.array([95, 200, 200])
+lower_red = np.array([165, 120, 100])
+upper_red = np.array([179, 255, 255])
 mouse.move(100, 100, absolute=True, duration=0.2)
 while True:
     cnt = []
